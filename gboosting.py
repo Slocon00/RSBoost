ef89462f6f64ab90dee878c7d7f780f19f8f2102
Slocon00 <79758160+Slocon00@users.noreply.github.com>
2025-01-29 10:01:19 +0100

    Add test on higgs dataset

--- gboosting.py
+++ gboosting.py
@@ -16,6 +16,7 @@ class _XGBTreeModel:
                  loss: str = 'mse',
                  algorithm: str = 'exact',
                  col_subsample: float = 1.0,
+                 row_subsample: float = 1.0,
                  seed: int = None
                  ):
         self.n_estimators = n_estimators
@@ -25,18 +26,19 @@ class _XGBTreeModel:
         self.lmbda = lmbda
         self.gamma = gamma
         
-        if loss == 'mse' or loss == 'logistic':
+        if loss in ['mse', 'logistic', 'pairwise']:
             self.loss = loss
         else:
             raise ValueError("Loss function must be either 'mse' or 'logistic'.")
 
-        if algorithm == 'exact' or algorithm == 'approx':
+        if algorithm in ['exact', 'approx']:
             self.algorithm = algorithm
         else:
             raise ValueError("Splitting algorithm must be either 'exact' or 'approx'.")
 
         self.generator = np.random.default_rng(seed)
         self.col_subsample = col_subsample
+        self.row_subsample = row_subsample
 
         self.ensemble = []
 
@@ -45,12 +47,16 @@ class _XGBTreeModel:
             return y - pred
         elif self.loss == 'logistic':
             return y - pred
+        elif self.loss == 'pairwise':
+            return y - pred
 
     def hessian(self, y: np.ndarray, pred: np.ndarray) -> np.ndarray:
         if self.loss == 'mse':
             return np.ones(y.shape[0])
         elif self.loss == 'logistic':
             return pred * (1 - pred)
+        elif self.loss == 'pairwise':
+            return np.ones(y.shape[0])
 
     def fit(self,
             X: np.ndarray,
@@ -66,6 +72,7 @@ class _XGBTreeModel:
 
         pred = np.array([self.starting_value] * X.shape[0])
         col_sample = np.arange(X.shape[1])
+        row_sample = np.arange(X.shape[0])
 
         for _ in range(self.n_estimators):
             residuals = y - pred
@@ -78,15 +85,25 @@ class _XGBTreeModel:
                 col_sample = self.generator.choice(X.shape[1],
                                                    int(X.shape[1] * self.col_subsample),
                                                    replace=False)
+            # row subsampling
+            if self.row_subsample < 1.0:
+                row_sample = self.generator.choice(X.shape[0],
+                                                   int(X.shape[0] * self.row_subsample),
+                                                   replace=False)
 
             tree = Tree(max_depth=self.max_depth,
                         lmbda=self.lmbda,
                         gamma=self.gamma,
                         algorithm=self.algorithm)
-            tree.fit(X[:,col_sample], residuals, self.gradient(y, pred), self.hessian(y, pred))
+            
+            tree.fit(X[row_sample,:][:,col_sample],
+                     residuals[row_sample],
+                     self.gradient(y[row_sample], pred[row_sample]),
+                     self.hessian(y[row_sample], pred[row_sample])
+                     )
 
             if self.loss == 'logistic':
-                # convert pred to log odds, add to prediction,4
+                # convert pred to log odds, add to prediction,
                 # then convert back to probability
                 pred = np.log(pred/(1-pred))
                 pred += self.eta * tree.predict(X)
@@ -128,6 +145,7 @@ class XGBTreeClassifier(_XGBTreeModel):
                  loss: str = 'logistic',
                  algorithm: str = 'exact',
                  col_subsample: float = 1.0,
+                 row_subsample: float = 1.0,
                  seed: int = None):
         super().__init__(n_estimators,
                          max_depth,
@@ -138,11 +156,16 @@ class XGBTreeClassifier(_XGBTreeModel):
                          loss,
                          algorithm,
                          col_subsample,
+                         row_subsample,
                          seed)
     
-    def fit(self, X: np.ndarray, y: np.ndarray, verbose: bool = False):
+    def fit(self,
+            X: np.ndarray,
+            y: np.ndarray,
+            verbose: bool = False,
+            thresh: float = 1e-4):
         '''Produce a fitted model.'''
-        super().fit(X, y, verbose)
+        super().fit(X, y, verbose, thresh)
 
     def predict_proba(self, X):
         '''Predict the probability of each class for each sample in X.'''
@@ -170,6 +193,7 @@ class XGBTreeRegressor(_XGBTreeModel):
                  loss: str = 'mse',
                  algorithm: str = 'exact',
                  col_subsample: float = 1.0,
+                 row_subsample: float = 1.0,
                  seed: int = None):
         super().__init__(n_estimators,
                          max_depth,
@@ -180,12 +204,58 @@ class XGBTreeRegressor(_XGBTreeModel):
                          loss,
                          algorithm,
                          col_subsample,
+                         row_subsample,
                          seed)
     
-    def fit(self, X: np.ndarray, y: np.ndarray, verbose: bool = False):
+    def fit(self,
+            X: np.ndarray,
+            y: np.ndarray,
+            verbose: bool = False,
+            thresh: float = 1e-4):
         '''Produce a fitted model.'''
-        super().fit(X, y, verbose)
+        super().fit(X, y, verbose, thresh)
 
     def predict(self, X):
         '''Predict regression value for each sample in X.'''
         return super().predict(X)
+    
+class XGBTreeRanker(_XGBTreeModel):
+    '''A class that represents a gradient boosting tree ranker
+    trained with the algorithm used by XGBoost.
+    '''
+    def __init__(self,
+                 n_estimators: int = 100,
+                 max_depth: int = 3,
+                 starting_value: float = 0.5,
+                 eta: float = 0.1,
+                 lmbda: float = 0.0,
+                 gamma: float = 0.0,
+                 loss: str = 'pairwise',
+                 algorithm: str = 'exact',
+                 col_subsample: float = 1.0,
+                 row_subsample: float = 1.0,
+                 seed: int = None):
+        super().__init__(n_estimators,
+                         max_depth,
+                         starting_value,
+                         eta,
+                         lmbda,
+                         gamma,
+                         loss,
+                         algorithm,
+                         col_subsample,
+                         row_subsample,
+                         seed)
+        
+    def fit(self, X: np.ndarray,
+            y: np.ndarray,
+            qid: np.ndarray,
+            verbose: bool = False,
+            thresh: float = 1e-4):
+        '''Produce a fitted model.'''
+        # TODO
+        super().fit(X, y, verbose, thresh)
+
+    def predict(self, X):
+        # TODO
+        pass
--- tree.py
+++ tree.py
@@ -33,12 +33,14 @@ class Tree:
                  max_depth: int = None,
                  lmbda: float = 0.0,
                  gamma: float = 0.0,
-                 algorithm: str = 'exact'):
+                 algorithm: str = 'exact',
+                 epsilon: float = 0.1):
         self.root = None
         self.max_depth = max_depth
         self.lmbda = lmbda
         self.gamma = gamma
         self.algorithm = algorithm
+        self.epsilon = epsilon
     
     def fit(self,
             X: np.ndarray,
@@ -140,41 +142,70 @@ class Tree:
         using the approximate algorithm, returning a tuple of feature index and
         threshold. If no such split exists, return None.
         '''
-        S = np.unique(X)
-        summary = (S,
-                   np.array([self._r_minus(X, y, x) for x in S]),
-                   np.array([self._r_plus(X, y, x) for x in S]),
-                   np.array([self._omega(X, y, x) for x in S]))
-        
-        # TODO
-        return None
+        # S = np.unique(X)
+        # summary = (S,
+        #           np.array([self._r_minus(X, hessians, x) for x in S]),
+        #           np.array([self._r_plus(X, hessians, x) for x in S]),
+        #           np.array([self._omega(X, hessians, x) for x in S]))
+        best_split = None
+        parent_sim = self._similarity_score(y, gradients, hessians)
 
-    def _r_minus(self, X: np.ndarray, y: np.ndarray, x: np.ndarray) -> float:
+        for i in range(X.shape[1]):
+            # find splits S = {s_1, s_2, ... , s_n} s.t.
+            # |rank(s_i) - rank(s_i+1)| < epsilon
+            splits = np.quantile(X[:,i],
+                                 np.linspace(0, 1, 1/self.epsilon),
+                                 weights=hessians)
+            
+            for split in splits:
+                left_y = y[X[:,i] <= split]
+                right_y = y[X[:,i] > split]
+
+                left_g = gradients[X[:,i] <= split]
+                right_g = gradients[X[:,i] > split]
+                left_h = hessians[X[:,i] <= split]
+                right_h = hessians[X[:,i] > split]
+
+                # if either leaf becomes empty skip
+                if len(left_y) < 1 or len(right_y) < 1:
+                    continue
+
+                left_sim = self._similarity_score(left_y, left_g, left_h)
+                right_sim = self._similarity_score(right_y, right_g, right_h)
+
+                if (left_sim + right_sim - parent_sim) - self.gamma > 0.0:
+                    # gain is positive, better than last best
+                    best_split = (i, split)
+                    parent_sim = (left_sim + right_sim) - self.gamma
+
+        return best_split
+
+    def _r_minus(self, X: np.ndarray, h: np.ndarray, x: np.ndarray) -> float:
         '''Return the rank of x, calculated as the sum of weights assigned to
         vectors in X that are strictly smaller than x.
         '''
-        return np.sum(y[X < x])
+        return np.sum(h[X < x]) if np.any(X < x) else 0
 
-    def _r_plus(self, X: np.ndarray, y: np.ndarray, x: np.ndarray) -> float:
+    def _r_plus(self, X: np.ndarray, h: np.ndarray, x: np.ndarray) -> float:
         '''Return the rank of x, calculated as the sum of weights assigned to
         vectors in X that are smaller than or equal to x.
         '''
-        return np.sum(y[X <= x])
+        return np.sum(h[X <= x]) if np.any(X <= x) else 0
     
-    def _omega(self, X: np.ndarray, y: np.ndarray, x: np.ndarray) -> float:
+    def _omega(self, X: np.ndarray, h: np.ndarray, x: np.ndarray) -> float:
         '''Return the weight of x, calculated as the sum of weights assigned to
         vectors in X that are equal to x.
         '''
-        return np.sum(y[X == x])
+        return np.sum(h[X == x]) if np.any(X == x) else 0
 
     def _merge_summaries(self, left: tuple, right: tuple) -> tuple:
         '''Merge the two summaries in a single one.'''
-        S1 = left[0]
-        S2 = right[0]
-
-        S = np.unique(np.concatenate((S1, S2)))
-        # TODO
-        pass
+        S = np.unique(np.concatenate((left[0], right[0])))
+        r_minus = None
+        r_plus = None
+        omega = None
+        
+        return (S, r_minus, r_plus, omega)
 
     def _prune_summary(self, summary: tuple) -> tuple:
         '''Prune the summary, reducing its size.'''
